ce9b9f5370e51e2558550ab1bd72e164ba561a39
valexxy <azunnaukah@gmail.com>
2026-09-19 15:52:18 +0100

    fix(ui): Adjust margin for PlasmaStreakAvatar to prevent overlap with neighboring header icons

--- components/stadium-header.tsx
+++ components/stadium-header.tsx
@@ -164,7 +164,7 @@ export const StadiumHeader: React.FC<StadiumHeaderProps> = ({ onOpenPlayers, cur
         <div className="flex items-center space-x-2 flex-shrink-0">
           {/* Matchday Web Push Notification Bell & Trigger Switch */}
           <MatchdayPushBell />
-          <div className="hidden sm:block ml-3"><PlasmaStreakAvatar userProfile={{username: 'Valexxy'}} /></div>
+          <div className="hidden sm:block mx-3"><PlasmaStreakAvatar userProfile={{username: 'Valexxy'}} /></div>
 
           {/* Global Language Switcher */}
           <GlobalLanguageSwitcher />
@@ -250,6 +250,7 @@ export const StadiumHeader: React.FC<StadiumHeaderProps> = ({ onOpenPlayers, cur
     </header>
   );
 };
+
 
 
 
